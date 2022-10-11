eefc501157e48fcf59eba4f64957548ca7298f1f
Jisan Hasan <jisan.jh5@gmail.com>
2022-10-11 21:49:45 +0600

    complete quiz page design

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -6,11 +6,15 @@ const Quiz = ({ quiz }) => {
   const qs = question.split("<p>").join("").split("</p>").join("");
 
   return (
-    <div className="bg-slate-300 p-5 rounded-xl w-2/3 mx-auto">
-      <h2 className="text-xl font-semibold">{qs}</h2>
-      <div>
+    <div className="bg-slate-300 p-5 rounded-xl">
+      <div className="text-center">
+      <h2 className="text-xl font-bold mb-3">{qs}</h2>
+      </div>
+      <div className="mx-5 grid gap-3">
         {options.map((option, idx) => (
-          <p className="" key={idx}>{option}</p>
+          <p className="bg-lime-400 px-8 py-5 rounded-xl text-lg font-semibold hover:cursor-pointer" key={idx}>
+            {option}
+          </p>
         ))}
       </div>
     </div>
--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -7,10 +7,12 @@ const Quizes = () => {
   const { total, name, questions } = loaderData.data;
   console.log(loaderData);
   return (
-    <div className="m-6">
-      <h2>Topic: {name}</h2>
-      <h2>Total Quizes {total}</h2>
-      <div className="grid grid-cols-1 gap-3">
+    <div className="m-6 w-2/3 mx-auto">
+      <div className="text-center">
+        <h2 className="text-2xl font-bold mb-2">Topic: {name}</h2>
+        <h2 className="text-lg font-semibold">Total Quizes {total}</h2>
+      </div>
+      <div className="grid grid-cols-1 gap-3 mt-6">
         {questions.map((quiz) => (
           <Quiz key={quiz.id} quiz={quiz} />
         ))}
