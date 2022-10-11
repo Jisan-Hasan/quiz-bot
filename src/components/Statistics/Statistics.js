9b690e00cb0baf704a5060911217522b0554c4e2
Jisan Hasan <jisan.jh5@gmail.com>
2022-10-12 02:29:59 +0600

    Statistics Chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { Tooltip } from "flowbite-react";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 const Statistics = () => {
-    return (
-        <div>
-            <h2>Statistics Section</h2>
-        </div>
-    );
+  const loaderData = useLoaderData();
+  const topicsData = loaderData.data;
+  const data = [];
+  topicsData.forEach((quiz) => {
+    const singleData = { name: quiz.name, total: quiz.total };
+    data.push(singleData);
+  });
+  console.log(topicsData);
+  return (
+    <div className="text-center">
+      <h2 className="text-2xl font-bold text-yellow-300">
+        Statistics of the Quizes
+      </h2>
+      <div className="flex justify-center mt-12">
+        <ResponsiveContainer width={400} height={400}>
+          <BarChart data={data}>
+            <Bar dataKey="total" fill="#8884d8" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
