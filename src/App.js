fe8d10252589c34fa984a8ef5e68c2f0d493e66d
Jisan Hasan <jisan.jh5@gmail.com>
2022-10-11 15:19:17 +0600

    navbar & route added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Statistics from "./components/Statistics/Statistics";
+import Topic from "./components/Topic/Topic";
+import Main from "./layouts/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <Topic />,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics />,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import { Navbar } from "flowbite-react";
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar fluid={false} rounded={true}>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+              Quiz Bot
+            </span>
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <NavLink to="/">
+            <Navbar.Link>Topic</Navbar.Link>
+          </NavLink>
+          <NavLink to="/statistics">
+            <Navbar.Link>Statistics</Navbar.Link>
+          </NavLink>
+          <NavLink to="/blogs">
+            <Navbar.Link>Blogs</Navbar.Link>
+          </NavLink>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
